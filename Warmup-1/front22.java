56577c7d8ebf7696cc500aa61f2ac1a6a7f74d53
zutesuit <neilsalvesen@gmail.com>
2023-08-08 05:32:57 +0100

    organised and done a few more warmups

---
+++ Warmup-1/front22.java
@@ -0,0 +1,8 @@
+// Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are there.
+
+public String front22(String str) {
+  
+  String front = str.length() >= 2 ? str.substring(0,2) : str;
+  return front + str + front;
+}
+
---
+++ Warmup-1/startHi.java
@@ -0,0 +1,5 @@
+// Given a string, return true if the string starts with "hi" and false otherwise.
+
+public boolean startHi(String str) {
+  return str.length() >= 2 && str.substring(0,2).equals("hi");
+}
